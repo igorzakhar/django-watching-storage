4d4a66960e32b8f7f1e1dc095f8b970f6c146601
Igor Zakharov <igorzakhar@gmail.com>
2022-12-18 23:03:32 +0300

    Add get_object_or_404 shortcut

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -1,10 +1,12 @@
+from django.shortcuts import get_object_or_404
+from django.shortcuts import render
+
 from datacenter.models import Passcard
 from datacenter.models import Visit
-from django.shortcuts import render
 
 
 def passcard_info_view(request, passcode):
-    passcard = Passcard.objects.filter(passcode=passcode)[0]
+    passcard = get_object_or_404(Passcard, passcode=passcode)
     visits = Visit.objects.filter(passcard=passcard)
 
     this_passcard_visits = [
